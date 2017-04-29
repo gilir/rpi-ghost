a5318ff191e7c1edcc111357d8dc71c21be11f80
Julien Lavergne <julien.lavergne@gmail.com>
2017-04-29 14:09:03 +0200

    Replace gosu by su-exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # allow the container to be started with `--user`
 if [[ "$*" == npm*start* ]] && [ "$(id -u)" = '0' ]; then
 	chown -R user "$GHOST_CONTENT"
-	exec gosu user "$BASH_SOURCE" "$@"
+	exec su-exec user "$BASH_SOURCE" "$@"
 fi
 
 if [[ "$*" == npm*start* ]]; then
